38a4d66e629abb982edbfc0fccda73ef4000d653
satish <seelamsettisatish247@gmail.com>
2020-12-20 10:03:38 +0530

    little Endian

---
+++ litte_endian.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+/*	int a=1;
+char *ptr=&a;
+
+if(*ptr==1)
+	printf("little endian\n")
+	else
+	printf("big endian\n")
+
+	}*/
+system("lscpu|grep Endian\n");
+}
